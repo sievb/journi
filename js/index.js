b54e934de073021e9e2dadf459572aaa669bab24
Benjamin Siev <benjaminsiev@Bento.T-mobile.com>
2019-04-15 22:00:51 -0700

    about team info popup works on simulated mobile, testing on actual device then moving on to make it responsive

--- js/index.js
+++ js/index.js
@@ -25,38 +25,78 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
-var personInfo = {
+var personInfo = [
     {
         "name": "Angela Birchman",
         "role": "Project Manager",
-        "portfolioLink": "https://www.google.com/"
+        "portfolioLink": "https://www.google.com/",
+        "img": "img/profile.png"
     },
 
     {
         "name": "Benjamin Siev",
         "role": "Web Developer ( ͡° ͜ʖ ͡°)",
-        "portfolioLink": "https://www.google.com/"
+        "portfolioLink": "https://www.google.com/",
+        "img": "img/profile.png"
     },
 
     {
         "name": "Junior Tran-Thien",
         "role": "Narrative Designer",
-        "portfolioLink": "https://www.google.com/"
+        "portfolioLink": "https://www.google.com/",
+        "img": "img/profile.png"
     },
 
     {
         "name": "Su Li",
         "role": "App Developer",
-        "portfolioLink": "https://www.google.com/"
+        "portfolioLink": "https://www.google.com/",
+        "img": "img/profile.png"
     },
 
     {
         "name": "Yin Yin",
         "role": "UI/UX Designer",
-        "portfolioLink": "https://www.google.com/"
+        "portfolioLink": "https://www.google.com/",
+        "img": "img/profile.png"
     }
-};
+];
 
-$(document).ready() {
-    console.log(personInfo[0].name);
+function openOverlay() {
+    document.getElementById("infoOverlay").style.height = "100%";
 }
+
+function closeOverlay() {
+    document.getElementById("infoOverlay").style.height = "0%";
+    $(".infoContent").empty();
+}
+
+var overlayWindow = document.getElementById("infoOverlay");
+
+window.onclick = function(event) {
+    if (event.target == overlayWindow) {
+        closeOverlay();
+    }
+}
+
+$(document).ready(function() {
+    var userSelection = document.getElementsByClassName('profilePics');
+
+    for(var i = 0; i < userSelection.length; i++) {
+        (function(index) {
+            userSelection[index].addEventListener("click", function() {
+            console.log("Clicked index: " + index);
+            console.log(personInfo[index].name);
+            openOverlay();
+
+            $(".infoContent").append(
+                "<img src='" + personInfo[index].img + "'/>",
+                "<p>" + personInfo[index].name +"</p>",
+                "<p>" + personInfo[index].role +"</p>",
+                "<a href='" + personInfo[index].portfolioLink + "'>google.com</a>"
+            );
+            })
+        })
+        (i);
+    }
+})
