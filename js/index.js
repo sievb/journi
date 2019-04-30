b2c3e4e893398cf47e7aacefc261858decb7cd13
Benjamin Siev <benjaminsiev@MacBook-Pro.T-mobile.com>
2019-04-29 22:20:37 -0700

    restructured image carousel, working on getting animation working

--- js/index.js
+++ js/index.js
@@ -9,21 +9,30 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+$(".prev").on("click", function() {
+    plusSlides(-1);
+    
+})
+
+$(".next").on("click", function() {
+    plusSlides(1);
+})
+
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("carouselImage");
+    var slideImages = $(".carouselImage img");
+    var caption = $(".caption");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1};
-    if (n < 1) {slideIndex = slides.length};
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+    if (n > slideImages.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = slideImages.length};
+    for (i = 0; i < slideImages.length; i++) {
+        slideImages[i].style.display = "none";  
+        caption[i].style.display = "none"
     };
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     };
-    slides[slideIndex-1].style.display = "flex";
+    slideImages[slideIndex-1].style.display = "flex";
+    caption[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 };
-
-var carouselImg = $(".carousel img");
-var imgCaption = $(".caption");
